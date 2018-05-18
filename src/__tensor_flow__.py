9683159622149cad0a8d2a86fa70e569996a3afe
Sanatan Das <sanatanonline@gmail.com>
2018-05-18 03:14:54 -0700

    python scripts updated

---
+++ src/__tensor_flow__.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a + b))
+
+sess.close()
+
