616f3332b1b4342cde5147ee94106aa7cbb7f3d1
Sanatan Das <sanatanonline@gmail.com>
2018-06-18 19:15:00 -0700

    files cleaned up and re-organized

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,2 @@
-from numpy import *
-a = array([[1., 2., 3.], [4., 5., 6.,], [7., 8., 9.0]])
+print("Hello Python!")
 
-print(a)
-print(a.shape)
-print(a.size)
-
-b = ones((3, 3, 3))
-print(b)
-
-m = matrix([[1., 2., 3.], [4., 5., 6.,], [7., 8., 9.0]])
-print(m)
