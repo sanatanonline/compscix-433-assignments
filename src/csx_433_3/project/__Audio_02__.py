81da91023d9a12c16c424b1d47664813c27de026
Sanatan Das <sanatanonline@gmail.com>
2018-06-27 14:28:03 -0700

    python scripts updated

---
+++ src/csx_433_3/project/__Audio_02__.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Graphing helper function
+def setup_graph(title='', x_label='', y_label='', fig_size=None):
+    fig = plt.figure()
+    if fig_size is not None:
+        fig.set_size_inches(fig_size[0], fig_size[1])
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+
+t = np.linspace(0, 3, 200)
+freq_1hz_amp_10 = 10 * np.sin(1 * 2*np.pi*t)
+freq_3hz_amp_5 = 5 * np.sin(3 * 2*np.pi*t)
+complex_wave = freq_1hz_amp_10 + freq_3hz_amp_5
+
+setup_graph(x_label='time (in seconds)', y_label='amplitude', title='original wave', fig_size=(12, 6))
+_ = plt.plot(t, complex_wave)
+plt.show()
+
+
+freq_1hz = np.sin(1 * 2*np.pi*t)
+setup_graph(x_label='time (in seconds)', y_label='amplitude', title='original wave * 1Hz wave', fig_size=(12,6))
+_ = plt.plot(t, complex_wave * freq_1hz)
+plt.show()
+
+print(sum(complex_wave * freq_1hz))
+print("Amplitude of 1hz component: ", sum(complex_wave*freq_1hz) * 2.0 * 1.0/len(complex_wave))
---
+++ src/csx_433_3/project/__Audio_03__.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import scipy.io.wavfile
+
+# Plot function for the graph
+def setup_graph(title='', x_label='', y_label='', fig_size=None):
+    fig = plt.figure()
+    if fig_size is not None:
+        fig.set_size_inches(fig_size[0], fig_size[1])
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+# stft function
+def stft(input_data, sample_rate, window_size, hop_size):
+    window = scipy.hamming(window_size)
+    output = scipy.array([scipy.fft(window*input_data[i:i+window_size])
+                         for i in range(0, len(input_data)-window_size, hop_size)])
+    return output
+
+
+def istft(input_data, sample_rate, window_size, hop_size, total_time):
+    output = scipy.zeros(int(total_time*sample_rate))
+    for n,i in enumerate(range(0, len(output)-window_size, hop_size)):
+        output[i:i+window_size] += scipy.real(scipy.ifft(input_data[n]))
+    return output
+
+
+def low_pass_filter(max_freq, window_size, sample_rate):
+    fft_bin_width = sample_rate / window_size
+    max_freq_bin = int(max_freq / fft_bin_width)
+    filter_block = np.ones(window_size)
+    filter_block[max_freq_bin:(window_size-max_freq_bin)] = 0
+    return filter_block
+
+
+def high_pass_filter(min_freq, window_size, sample_rate):
+    return np.ones(window_size) - low_pass_filter(min_freq, window_size, sample_rate)
+
+
+def write_audio_file(filename, filedata, sample_rate):
+    scipy.io.wavfile.write(filename, sample_rate, filedata)
+
+
+def filter_audio(input_signal, sample_rate, filter_window, window_size=256):
+    # Setting parameters
+    hop_size = window_size // 2
+    total_time = len(input_signal) / sample_rate
+
+    # Do actual filtering
+    stft_output = stft(input_signal, sample_rate, window_size, hop_size)
+    filtered_result = [original * filter_window for original in stft_output]
+    resynth = istft(filtered_result, sample_rate, window_size, hop_size, total_time)
+
+    return resynth
+
+
+infile = "audio_files/ohm_scale.wav"
+outfile = "audio_files/high_pass_out.wav"
+window_size = 256
+
+# Input
+(sample_rate, input_signal) = scipy.io.wavfile.read(infile)
+
+# Create filter window
+filter_window = high_pass_filter(2500, window_size, sample_rate)
+
+# Run filter
+resynth = filter_audio(input_signal, sample_rate, filter_window, window_size)
+
+# Output
+write_audio_file(outfile, resynth, sample_rate)
+
+setup_graph(title='Spectrogram (Before)', x_label='time (in seconds)', y_label='frequency', fig_size=(14,7))
+_ = plt.specgram(input_signal, Fs=sample_rate)
+plt.show()
+
+setup_graph(title='Spectrogram (After)', x_label='time (in seconds)', y_label='frequency', fig_size=(14,7))
+_ = plt.specgram(resynth, Fs=sample_rate)
+plt.show()
+
+setup_graph(title='Sound wave (Before)', x_label='time (in seconds)', y_label='amplitude', fig_size=(14,7))
+_ = plt.plot(input_signal)
+plt.show()
+
+setup_graph(title='Sound wave (After)', x_label='time (in seconds)', y_label='amplitude', fig_size=(14,7))
+_ = plt.plot(resynth)
+plt.show()
+
+infile = "audio_files/doremi_xylo.wav"
+outfile = "audio_files/low_pass_out.wav"
+window_size = 256
+
+# Input
+(sample_rate, input_signal) = scipy.io.wavfile.read(infile)
+
+# Create filter window
+filter_window = low_pass_filter(1700, window_size, sample_rate)
+
+# Run filter
+resynth = filter_audio(input_signal, sample_rate, filter_window, window_size)
+
+# Output
+write_audio_file(outfile, resynth, sample_rate)
